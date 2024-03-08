0e4790ef15685cc26c7359e54647e717c320485e
dthiel22 <113801402+dthiel22@users.noreply.github.com>
2024-03-07 20:21:31 -0800

    last push

--- components/home-page-components/StatsTable.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function StatsTable({collectionStatsData}) {
-    console.log(collectionStatsData)
-  return (
-    <div>StatsTable</div>
-  )
-}
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ function AllComponents({  }) {
   return (
     <>
     <div 
+    key={"top div"}
      className="flex fixed top-0 left-0 right-0 bottom-0 bg-slate-900 z-[999] justify-center items-center">
        <div 
        className="flex flex-col w-3/4 lg:w-3/5 h-[70vh] lg:max-h-3/4 border-4 border-black rounded-lg shadow-lg fixed bg-gray-100 z-[1000] overflow-y-auto">
@@ -17,8 +18,8 @@ function AllComponents({  }) {
                <div className="flex flex-wrap justify-around">
                  <NewsDiv date={'March 7th, 2024'} information={
                      <>
-                      <p className="px-2 card-sm:px-4 py-2 text-md card-sm:text-base font-bold indent-4">It is with a sad heart that I have come to say that I will be closing down Dokkan Battle Helper. It was such a blast to make and critique the site based upon your guys' needs...then hear back about how much you liked the site and how helpful it was. This was the first website I had ever built and I am pretty proud of it. The reason I am closing it down is multiple reasons. The site became too time-consuming, as some of you can see, I didn't update the characters for over 4 months, and I never got the chance to automate character generating as other sites have. I got a new job 4 months ago and figured I would be able to keep up with my workload while also maintaining the site, but I was incredibly wrong. On top of that, to keep the website running became too expensive.</p>
-                      <p className="px-2 card-sm:px-4 py-2 text-md card-sm:text-base font-bold indent-4">I learned so much from building out this website and I truly thank each and every one of you for using it. I hope you enjoyed it and found it as useful as I did. Sites that are incredibly useful and very well-maintained include <a className='text-blue-500' href={`https://dokkan.wiki/`}>Dokkan Wiki</a> (the creator of this website was incredibly helpful while I was making mine, please support his site, it's incredibly well made) and <a className='text-blue-500' href={`https://dokkantools.com/`}>Dokkan Tools</a>, which is a website that functioned a lot like mine and will help your generate teams. My only suggestion to the Dokkan Tools creator would be to have the ability to see character stat gains from their links + team saving.</p>
+                      <p className="px-2 card-sm:px-4 py-2 text-md card-sm:text-base font-bold indent-4">It is with a sad heart that I have come to say that I will be closing down Dokkan Battle Helper. It was such a blast to make and critique the site based upon your guys needs...then hear back about how much you liked the site and how helpful it was. This was the first website I had ever built and I am pretty proud of it. The reason I am closing it down is multiple reasons. The site became too time-consuming, as some of you can see, I did not update the characters for over 4 months, and I never got the chance to automate character generating as other sites have. I got a new job 4 months ago and figured I would be able to keep up with my workload while also maintaining the site, but I was incredibly wrong. On top of that, to keep the website running became too expensive.</p>
+                      <p className="px-2 card-sm:px-4 py-2 text-md card-sm:text-base font-bold indent-4">I learned so much from building out this website and I truly thank each and every one of you for using it. I hope you enjoyed it and found it as useful as I did. Sites that are incredibly useful and very well-maintained include <a className='text-blue-500' href={`https://dokkan.wiki/`}>Dokkan Wiki</a> (the creator of this website was incredibly helpful while I was making mine, please support his site, it is incredibly well made) and <a className='text-blue-500' href={`https://dokkantools.com/`}>Dokkan Tools</a>, which is a website that functioned a lot like mine and will help your generate teams. My only suggestion to the Dokkan Tools creator would be to have the ability to see character stat gains from their links + team saving.</p>
                       <p className="px-2 card-sm:px-4 py-2 text-md card-sm:text-base font-bold indent-4">Again, thank you for using and contributing to this site. I will be keeping my eye on Dokkan and if the Japan and Global database ever do end up merging, I will debate working slowly to generate a team-building app that is not expensive to maintain and also can automatically be updated. However, it seems like Dokkan Tools is close to accomplishing this! If you ever want to reach me, I casually check this email: matt.alex.projects@gmail.com</p>
 
                       <break></break>
@@ -26,7 +27,7 @@ function AllComponents({  }) {
                       <p className="px-2 card-sm:px-4 py-2 text-md card-sm:text-base font-bold indent-4">Take care.</p>
                      </>
                    }
-                   key={'Update 1'}
+                   key={"Update 1"}
                  />
                </div>
                </div>
